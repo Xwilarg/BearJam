d9b2dda3aae0b0aa493f06def3b7ae796d28a9f9
Luxio-Chaosyn <107632641+Luxio-Chaosyn@users.noreply.github.com>
2022-10-29 18:27:15 +0200

    Add timer to night system and Gamesystem object

---
+++ Scripts/GameSytem.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var clock = Timer.new()
+
+var day_time = 0; # day = 0, night = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	clock.one_shot = true;
+	clock.connect("timeout", self, "change_to_night");
+	clock.start(600);
+	pass # Replace with function body.
+
+func _change_to_night():
+	day_time = 1;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
