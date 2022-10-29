d3e3952550f799da74423c8efe16cac529860114
Xwilarg <xwilarg@protonmail.com>
2022-10-29 22:40:24 +0200

    Clean code

--- Scripts/Night.gd
+++ Scripts/Night.gd
@@ -1,22 +1,14 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var perfect = 0;
 var hit = 0;
 var miss = 0;
 var combo = 0;
 var best_combo = 0;
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	hit = check_note($"Hit Area", hit);
 	perfect = check_note($"Perfect Area", perfect);
@@ -32,39 +24,18 @@ func update_ui():
 func check_note(zone, score):
 	var notes = zone.get_overlapping_bodies();
 	
-	for note in notes:
-		if note.translation.x == -1.5:
-			if Input.is_action_pressed("Red"):
-				score += 1;
-				combo += 1;
-				if combo > best_combo:
-					best_combo = combo;
-				note.free();
-			print("gauche");
-		elif note.translation.x == -0.5:
-			if Input.is_action_pressed("Blue"):
-				score += 1;
-				combo += 1;
-				if combo > best_combo:
-					best_combo = combo;
-				note.free();
-			print("centre gauche");
-		elif note.translation.x == 0.5:
-			if Input.is_action_pressed("Green"):
-				score += 1;
-				combo += 1;
-				if combo > best_combo:
-					best_combo = combo;
-				note.free();
-			print("centre droite");
-		elif note.translation.x == 1.5:
-			if Input.is_action_pressed("Yellow"):
-				score += 1;
-				combo += 1;
-				if combo > best_combo:
-					best_combo = combo;
-				note.free();
-			print("droite");
+	var xOffset = -1.5
+	for i in 4:
+		for note in notes:
+			if note.translation.x == xOffset + i:
+				for y in 4:
+					if Input.is_action_pressed("Line_" + str(y)):
+						score += 1;
+						combo += 1;
+						if combo > best_combo:
+							best_combo = combo;
+						notes.erase(note)
+						note.free();
 	return(score);
 
 func _on_Miss_Area_body_entered(body):
