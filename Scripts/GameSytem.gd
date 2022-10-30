c1aa8cab24d7fc6c1a129291c5c8b1028d3b2f75
Xwilarg <xwilarg@protonmail.com>
2022-10-30 19:33:32 +0100

    Change settings depending of resources

--- Scripts/GameSytem.gd
+++ Scripts/GameSytem.gd
@@ -11,15 +11,20 @@ var note_speed = 10;
 var finalScore: int
 
 enum ResourceType { WOOD, ROCK, SCRAP, CANDY}
-var residents_ressources = [0, 0, 0, 0];
+var residents_ressources = [0, 0, 0, 0]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func updateMainParameters():
+	var wood = residents_ressources[ResourceType.WOOD]
+	var rock = residents_ressources[ResourceType.ROCK]
+	var scrap = residents_ressources[ResourceType.SCRAP]
+	var candy = residents_ressources[ResourceType.CANDY]
+	if candy > 10:
+		candy = 10
+	permissiveness = 1 - (.1 * candy)
+	maxBurst = 1 + (rock / 10)
+	delayBetweenNotes = 0.2 - (0.01 * scrap)
+	songDuration = 10 + (5 * wood)
 
 func go_to_scene(path):
 	get_tree().change_scene(path)
 	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
